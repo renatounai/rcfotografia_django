12d802758a0f6e5b1c7e82974c14200cc7fa8e05
Renato Custódio Pereira <renatomcn@gmail.com>
2022-10-26 12:12:04 -0300

    Documentação de TODOs

--- photos/views.py
+++ photos/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
-# a view for the index.html page
+# TODO - Alterar para usar Class Based Views
+# TODO - Passar a colocar cada view em um arquivo separado
 def index(request):
     return render(request, "photos/index.html")
 
