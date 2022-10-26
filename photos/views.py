420bf3dcfe36f4a603b4c792bbbbd3cccd418bd3
Renato Custódio Pereira <renatomcn@gmail.com>
2022-10-25 23:35:34 -0300

    estruturacao da galeria

--- photos/views.py
+++ photos/views.py
@@ -7,6 +7,3 @@ from django.shortcuts import render
 def index(request):
     return render(request, "photos/index.html")
 
-
-def private(request):
-    return render(request, "photos/private.html")
