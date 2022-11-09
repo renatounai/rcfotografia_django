429dad4daef0e8ac87eb824a66e57be701984df8
Renato Custódio Pereira <renatomcn@gmail.com>
2022-11-08 22:18:11 -0300

    Melhoria no elemento invisivel quando não é multiplo de 3

--- photos/views/index.py
+++ photos/views/index.py
@@ -10,11 +10,12 @@ class IndexView(TemplateView):
     def get(self, request):
         photos = Photo.objects.all()
         padding = 3 - len(photos) % 3
+        if padding == 3:
+            padding = 0
 
-        photos_padding = []
-        if padding < 3:
-            for i in range(padding):
-                photos_padding.append(Photo())
+        context = {
+            'photos': photos,
+            'padding': range(padding)
+        }
 
-        context = {'photos': photos, 'photos_padding': photos_padding}
         return render(request, self.template_name, context)
