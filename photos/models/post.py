35c946ea6adcba1da4e2c0f4b6c363b36c9fa833
Renato Custódio Pereira <renatomcn@gmail.com>
2022-11-11 10:46:47 -0300

    Process the tags after saving a post

--- photos/models/post.py
+++ photos/models/post.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from photos.models.Tag import Tag
 from photos.models.base import BaseModel
 from rcfotografia_django import settings
 
@@ -7,3 +8,16 @@ from rcfotografia_django import settings
 class Post(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
     text = models.TextField(blank=True, null=False, default='', max_length=1000)
+
+    def after_save(self):
+        from photos.models.TagPost import TagPost
+
+        # TODO - change this to a queue
+        TagPost.objects.filter(post=self).delete()
+
+        tags_in_the_current_text = [tag for tag in self.text.split() if tag.startswith('#')]
+        tags = [Tag.objects.get_or_create(name=tag) for tag in tags_in_the_current_text]
+        for tag in tags:
+            TagPost.objects.create(tag=tag, post=self)
+
+        super().after_save()
