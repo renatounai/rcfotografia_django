4093e5d76aa4fd48551d8df50ff69feef50786bf
Renato Custódio Pereira <renatomcn@gmail.com>
2022-11-11 09:53:24 -0300

    Login validation with javascript

--- photos/static/account/js/login.js
+++ photos/static/account/js/login.js
@@ -1,3 +1,19 @@
 function logout() {
     document.getElementById('signOutBtn').click();
 }
+
+function validate() {
+    let login = document.getElementById('id_login').value;
+    let password = document.getElementById('id_password').value;
+
+    if (login.trim() === "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    if (password.trim() === "") {
+        alert("Password must be filled out");
+        return false;
+    }
+
+    return true;
+}
